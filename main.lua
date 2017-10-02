b48289effb597f9a9f7ae8326add31e764577da6
schaerenD <daniel.schaeren@gmail.com>
2017-10-02 18:05:49 +0200

    Update 18

--- main.lua
+++ main.lua
@@ -16,7 +16,23 @@ end
 
 
 function MatchString(myString)
-  --local myString = "Chris,Lars"
+
+
+function Befehlsdecoder (Befehl)
+    
+end
+
+    local StringIn = "Chris,Lars,Daniel"
+    Richtung = "set"
+    Befehl = {}
+    local Wert
+     
+    --Test();
+    Wert = MatchString("Chris,Lars,Daniel");
+    --Befehlsdecoder (Befehl)
+    print(Befehl[0])
+    --print(maximum({8,10,23,12,5}))     --> 23   3 
+    --print (Befehl)  --local myString = "Chris,Lars"
   local i = 0
   
 	  for word in string.gmatch(myString, '([^,]+)') do
