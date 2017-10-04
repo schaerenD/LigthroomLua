7c00db720ea9227d2ed11b46aaec8defcf4aa320
root <schaerenD>
2017-10-04 13:22:38 +0200

    Start with LR get Function

--- main.lua
+++ main.lua
@@ -14,43 +14,67 @@ function Test()
 	  print( "Hallo Welt" )
 end
 
+----------------- set LR Function -----------------
+
+function Test()
+	  print( "Hallo Welt" )
+end
+
+----------------- set LR Function -----------------
+
+----------------- get LR Function -----------------
+
+function LR_Get_Temperatur()
+	  
+	  local Temperature = LrDevelopController.getValue("Temperature")
+	  return Temperature
+	  
+end
+
+function LR_Get_Temperatur()
+	  
+	  local Temperature = LrDevelopController.getValue("Temperature")
+	  return Temperature
+	  
+end
+
+----------------- get LR Function -----------------
+
+function Test()
+	  print( "Hallo Welt" )
+end
 
 function MatchString(myString)
-
-
-function Befehlsdecoder (Befehl)
-    
-end
-
-    local StringIn = "Chris,Lars,Daniel"
-    Richtung = "set"
-    Befehl = {}
-    local Wert
      
-    --Test();
-    Wert = MatchString("Chris,Lars,Daniel");
-    --Befehlsdecoder (Befehl)
-    print(Befehl[0])
-    --print(maximum({8,10,23,12,5}))     --> 23   3 
-    --print (Befehl)  --local myString = "Chris,Lars"
+   
+  Wert = MatchString("Chris,Lars,Daniel");
+    
   local i = 0
+  local Befehlswert = {}
   
-	  for word in string.gmatch(myString, '([^,]+)') do
-	    Befehl[i] = word
+	 for word in string.gmatch(myString, '([^,]+)') do
+	    Befehlswert[i] = word
 	    i = i + 1
 	    print(word)
-    end
-    return "Hello"
+   end
+   
+   Richtung = Befehlswert[0]
+   Befehl = Befehlswert[1]
+   Wert = Befehlswert[2]
+   
+   return "Done"
     
 end
 
 function Befehlsdecoder (Befehl)
+
+			
     
 end
 
     local StringIn = "Chris,Lars,Daniel"
     Richtung = "set"
-    Befehl = {}
+    Befehl = {}					--Befehlsarray
     local Wert
      
     --Test();
