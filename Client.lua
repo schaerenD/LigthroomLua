cdccc9bc043954333a3810bc71f3082557e0415b
root <schaerenD>
2017-11-21 14:14:33 +0100

    Test auf Tab S3

--- Client.lua
+++ Client.lua
@@ -21,9 +21,35 @@ onConnected = function( socket, port )
 end,
 onMessage = function( socket, message )
 -- Fakultaet(message)
-if message =="+" then LrDevelopController.increment ( "Exposure" ) end
-if message =="-" then LrDevelopController.decrement ( "Exposure" ) end
-
+			if     Befehl == "Contrast" then print("aah")
+			elseif Befehl == "Shadows" then print("bee")
+			elseif Befehl == "Blacks" then print("see")
+			elseif Befehl == "Whites" then print("dee")
+			elseif Befehl == "Hightigths" then print("eee")
+    elseif Befehl == "f" then print("eff")
+    elseif Befehl == "g" then print("gee")
+    elseif Befehl == "h" then print("aych")
+    elseif Befehl == "i" then print("eye")
+    elseif Befehl == "j" then print("jay")
+    elseif Befehl == "k" then print("kay")
+    elseif Befehl == "l" then print("el")
+    elseif Befehl == "m" then print("em")
+    elseif Befehl == "n" then print("en")
+    elseif Befehl == "o" then print("ooh")
+    elseif Befehl == "p" then print("pee")
+    elseif Befehl == "q" then print("queue")
+    elseif Befehl == "r" then print("arr")
+    elseif Befehl == "s" then print("ess")
+    elseif Befehl == "t" then print("tee")
+    elseif Befehl == "u" then print("you")
+    elseif Befehl == "v" then print("vee")
+    elseif Befehl == "w" then print("doubleyou")
+    elseif Befehl == "x" then print("ex")
+    elseif Befehl == "y" then print("why")
+    elseif Befehl == "z" then print(is_canadian and "zed" or "zee")
+    elseif Befehl == "?" then print(is_canadian and "eh" or "")
+    else                 print("blah")
+    end
 end,
 onClosed = function( socket )
 running = false
